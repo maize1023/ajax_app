b6e88f14b2a00104d4745fc2aefe4a01727c1188
Daiki Iwata <direction2423@gmail.com>
2022-10-30 12:09:35 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,6 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+    @posts = Post.order(id: "DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
   post 'posts', to: 'posts#create'
+
+  def create
+   Post.create(content: params[:content])
+   redirect_to action: :index  # 追記する
+ end
+
 end
