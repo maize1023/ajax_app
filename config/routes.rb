0882edc0c32c4dbed385eb3b86e4b66abe32d89d
Daiki Iwata <direction2423@gmail.com>
2022-10-31 10:52:22 +0900

    メモ投稿機能
    コントローラーにリクエストを送信する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
-
-  def create
-   Post.create(content: params[:content])
-   redirect_to action: :index  # 追記する
- end
-
 end
